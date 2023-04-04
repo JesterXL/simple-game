568d167210ba8ea189c2eb8b1d16d52ef6fa1324
Jesse Warden <jesterxl@jessewarden.com>
2023-04-04 07:39:09 -0400

    format and pausing

--- src/Main.elm
+++ src/Main.elm
@@ -1,33 +1,35 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, button, div, img)
-import Html.Events exposing (onClick, onMouseDown, onMouseUp )
-import Html.Attributes exposing (src, style, class)
-import Random
-import Time
-import Color
-import Browser.Events exposing (onAnimationFrameDelta)
+import Browser.Events exposing (Visibility(..), onAnimationFrameDelta, onVisibilityChange)
 import Canvas exposing (rect, shapes)
 import Canvas.Settings exposing (fill)
-import Canvas.Settings.Advanced exposing (rotate, transform, translate)
+import Color
+import Html exposing (Html, button, div)
+import Html.Attributes exposing (style)
+import Html.Events exposing (onClick, onMouseDown, onMouseUp)
+import Random
+
 
 type alias Model =
     { paused : Bool
     , gravity : Float
-    , frameNo : Int
+    , frame : Int
     , mySquare : Component
     , walls : List Component
     , wallTimePassed : Float
     , initialSeed : Random.Seed
     , currentSeed : Random.Seed
-    , gameOver : Bool }
+    , gameOver : Bool
+    , screenHidden : Bool
+    }
+
 
 initialModel : Random.Seed -> Model
 initialModel seed =
     { paused = False
     , gravity = 0.05
-    , frameNo = 0
+    , frame = 0
     , mySquare =
         { type_ = ""
         , score = 0
@@ -38,12 +40,16 @@ initialModel seed =
         , x = 10
         , y = 120
         , gravity = 0.05
-        , gravitySpeed = 0 }
+        , gravitySpeed = 0
+        }
     , walls = []
     , wallTimePassed = 9001.0
     , initialSeed = seed
     , currentSeed = seed
-    , gameOver = False }
+    , gameOver = False
+    , screenHidden = False
+    }
+
 
 type alias Component =
     { type_ : String
@@ -55,78 +61,116 @@ type alias Component =
     , x : Float
     , y : Float
     , gravity : Float
-    , gravitySpeed : Float}
+    , gravitySpeed : Float
+    }
+
 
 type Msg
-    = TogglePause
-    | Frame Float
-    | Accelerate Float
+    = TogglePause -- pause and unpause the game
+    | Frame Float -- the browser ticks; sends this every time it's ready to update an animation frame
+    | Accelerate Float -- accelerate the square up or down
+    | VisibilityChange Visibility -- when the browser tab is hidden or shown
 
-update : Msg -> Model -> (Model, Cmd Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         TogglePause ->
-            ( { model | paused = not model.paused }, Cmd.none)
+            ( { model | paused = not model.paused }, Cmd.none )
+
         Frame timePassed ->
             let
-                updatedFrameNo = 
-                    model.frameNo + 1
+                updatedframe =
+                    model.frame + 1
+
                 updatedWallTimePassed =
                     model.wallTimePassed + timePassed
-                (randomNum, newSeed) =
+
+                ( randomNum, newSeed ) =
                     Random.step (Random.float 0.0 1.0) model.currentSeed
-                wallsHit =
+
+                hitAnyWalls =
                     List.filter
                         (\wall -> testCrash model.mySquare wall)
                         model.walls
-                    |> List.length
-                hitAnyWalls =
-                    wallsHit > 0
+                        |> List.length
+                        |> (\length -> length > 0)
             in
             if hitAnyWalls == True then
-                ( { model | 
-                    gameOver = True
+                ( { model
+                    | gameOver = True
                     , paused = True
-                    , frameNo = updatedFrameNo
+                    , frame = updatedframe
                     , wallTimePassed = updatedWallTimePassed
                     , mySquare = updateSquarePosition model.mySquare
                     , walls = updateWalls randomNum False model.walls
-                    , currentSeed = newSeed }
-                , Cmd.none )
-            else if updatedWallTimePassed < 2500 then
-                ( { model | 
-                    frameNo = updatedFrameNo
+                    , currentSeed = newSeed
+                  }
+                , Cmd.none
+                )
+
+            else if updatedWallTimePassed < newWallDrawTime then
+                ( { model
+                    | frame = updatedframe
                     , wallTimePassed = updatedWallTimePassed
                     , mySquare = updateSquarePosition model.mySquare
                     , walls = updateWalls randomNum False model.walls
-                    , currentSeed = newSeed }
-                , Cmd.none )
+                    , currentSeed = newSeed
+                  }
+                , Cmd.none
+                )
+
             else
-                ( { model | 
-                    frameNo = updatedFrameNo
+                ( { model
+                    | frame = updatedframe
                     , wallTimePassed = 0
                     , mySquare = updateSquarePosition model.mySquare
                     , walls = updateWalls randomNum True model.walls
-                    , currentSeed = newSeed }
-                , Cmd.none )
+                    , currentSeed = newSeed
+                  }
+                , Cmd.none
+                )
+
         Accelerate amount ->
             if amount > 0 then
+                -- if you are going up, update your speed so it slowly floats up and stops,
+                -- otherwise you'll just keep floating up. If I increase gravity too much, you fall too fast.
                 ( { model | mySquare = updateSquareGravity model.mySquare amount |> resetSquareGravitySpeed }, Cmd.none )
+
             else
                 ( { model | mySquare = updateSquareGravity model.mySquare amount }, Cmd.none )
+
+        VisibilityChange vis ->
+            case vis of
+                Hidden ->
+                    ( { model | paused = True, screenHidden = True }, Cmd.none )
+
+                Visible ->
+                    ( { model | paused = False, screenHidden = False }, Cmd.none )
 
 
 updateWalls : Float -> Bool -> List Component -> List Component
 updateWalls randomNum makeNewWall walls =
     if makeNewWall then
         let
-            x = canvasWidth
-            minHeight = 20
-            maxHeight = 200
-            height = randomNum * (maxHeight - minHeight + 1) + minHeight
-            minGap = 50
-            maxGap = 200
-            gap = randomNum * (maxGap - minGap + 1) + minGap
+            minHeight =
+                20
+
+            maxHeight =
+                200
+
+            height =
+                randomNum * (maxHeight - minHeight + 1) + minHeight
+
+            minGap =
+                50
+
+            maxGap =
+                200
+
+            gap =
+                randomNum * (maxGap - minGap + 1) + minGap
+
             topWall =
                 { type_ = "wall"
                 , score = 0
@@ -134,37 +178,44 @@ updateWalls randomNum makeNewWall walls =
                 , height = height
                 , speedX = -1
                 , speedY = 0
-                , x = x
+                , x = canvasWidth
                 , y = 0.0
                 , gravity = 0
-                , gravitySpeed = 0 }
+                , gravitySpeed = 0
+                }
+
             bottomWall =
                 { type_ = "wall"
                 , score = 0
                 , width = 10
-                , height = x - height - gap
+                , height = canvasWidth - height - gap
                 , speedX = -1
                 , speedY = 0
-                , x = x
+                , x = canvasWidth
                 , y = height + gap
                 , gravity = 0
-                , gravitySpeed = 0 }
+                , gravitySpeed = 0
+                }
         in
-        walls ++ [topWall, bottomWall]
-    else
-        walls
-        |> List.map (\wall -> { wall | x = wall.x - 1 })
-        |> List.filter (\wall -> wall.x + wall.width > 0)
+        walls ++ [ topWall, bottomWall ]
 
+    else
+        -- move all walls left, then
+        -- remove walls that are off the screen
+        walls
+            |> List.map (\wall -> { wall | x = wall.x - 1 })
+            |> List.filter (\wall -> wall.x + wall.width > 0)
 
 
 updateSquareGravity : Component -> Float -> Component
 updateSquareGravity square gravity =
     { square | gravity = gravity }
 
+
 resetSquareGravitySpeed : Component -> Component
 resetSquareGravitySpeed square =
-    { square | gravitySpeed = -1.5}
+    { square | gravitySpeed = -1.5 }
+
 
 updateSquarePosition : Component -> Component
 updateSquarePosition square =
@@ -174,47 +225,62 @@ updateSquarePosition square =
     in
     if square.gravity > 0 && hitBottom square then
         { updatedSquare | gravitySpeed = 0, y = canvasHeight - square.height }
+
     else
         updatedSquare
 
+
+
+-- Copilot wrote this function and I hate how it looks
+
+
 testCrash : Component -> Component -> Bool
 testCrash square wall =
-    square.x < wall.x + wall.width &&
-    square.x + square.width > wall.x &&
-    square.y < wall.y + wall.height &&
-    square.y + square.height > wall.y
+    square.x
+        < wall.x
+        + wall.width
+        && square.x
+        + square.width
+        > wall.x
+        && square.y
+        < wall.y
+        + wall.height
+        && square.y
+        + square.height
+        > wall.y
+
 
 view : Model -> Html Msg
 view model =
-    div [][
-        div [][Html.text ("Score:" ++ (String.fromInt model.frameNo))]
+    div []
+        [ div [] [ Html.text ("Score:" ++ String.fromInt model.frame) ]
         , div
             [ style "display" "flex"
             , style "justify-content" "center"
             , style "align-items" "center"
             ]
-            (
-                Canvas.toHtml
-                    ( (round canvasWidth), (round canvasHeight) )
-                    [ style "border" "1px solid rgba(0,0,0,0.1)" ]
-                    (
-                        clearScreen
-                        :: componentToRect model.mySquare
-                        :: drawWalls model.walls
-                    )
-                
+            (Canvas.toHtml
+                ( round canvasWidth, round canvasHeight )
+                [ style "border" "1px solid rgba(0,0,0,0.1)" ]
+                (clearScreen
+                    :: componentToRect model.mySquare
+                    :: drawWalls model.walls
+                )
                 :: viewGameButtons model
             )
-    ]
+        ]
+
 
 viewGameButtons : Model -> List (Html Msg)
 viewGameButtons model =
     if model.gameOver == False then
         [ button [ onClick TogglePause ] [ Html.text "Pause" ]
-        , button [ onMouseDown (Accelerate -0.2), onMouseUp (Accelerate 0.05) ] [ Html.text "Accelerate" ] 
+        , button [ onMouseDown (Accelerate -0.2), onMouseUp (Accelerate 0.05) ] [ Html.text "Accelerate" ]
         ]
+
     else
-        [ div [][Html.text "Game Over"]]
+        [ div [] [ Html.text "Game Over" ] ]
+
 
 componentToRect : Component -> Canvas.Renderable
 componentToRect component =
@@ -222,10 +288,12 @@ componentToRect component =
         [ fill (Color.hsl 0.5 0.3 0.7) ]
         [ rect ( component.x, component.y ) component.width component.height ]
 
+
 drawWalls : List Component -> List Canvas.Renderable
 drawWalls walls =
     walls
-    |> List.map wallToRect
+        |> List.map wallToRect
+
 
 wallToRect : Component -> Canvas.Renderable
 wallToRect wall =
@@ -233,86 +301,70 @@ wallToRect wall =
         [ fill (Color.rgb255 0 0 1) ]
         [ rect ( wall.x, wall.y ) wall.width wall.height ]
 
+
 canvasWidth : Float
 canvasWidth =
     480
+
 
 canvasHeight : Float
 canvasHeight =
     270
 
-centerX : Float
-centerX =
-    canvasWidth / 2
 
+newWallDrawTime : Float
+newWallDrawTime =
+    2500
 
-centerY : Float
-centerY =
-    canvasHeight / 2
 
 clearScreen : Canvas.Renderable
 clearScreen =
     shapes [ fill Color.white ] [ rect ( 0, 0 ) canvasWidth canvasHeight ]
 
+
 newPos : Component -> Component
 newPos component =
-    { component 
-        | gravitySpeed = component.gravitySpeed + component.gravity 
-        , x = component.x + component.speedX 
-        , y = component.y + component.speedY + component.gravitySpeed }
+    { component
+        | gravitySpeed = component.gravitySpeed + component.gravity
+        , x = component.x + component.speedX
+        , y = component.y + component.speedY + component.gravitySpeed
+    }
+
 
 hitBottom : Component -> Bool
 hitBottom component =
-    let
-        rockBottom =
-            canvasHeight - component.height
-    in
     if component.y > canvasHeight - component.height then
         True
+
     else
         False
 
-collides : Component -> Component -> Bool
-collides component otherComponent =
-    let
-        bottom =
-            component.y + component.height
-        top =
-            component.y
-        left =
-            component.x
-        right =
-            component.x + component.width
-
-        otherTop =
-            otherComponent.y
-        otherBottom =
-            otherComponent.y + otherComponent.height
-        otherLeft =
-            otherComponent.x
-        otherRight =
-            otherComponent.x + otherComponent.width
-        
-    in
-    if ((bottom < otherTop) || (top > otherBottom) || (right < otherLeft) || (left > otherLeft)) then
-        False
-    else
-        True
 
 
 -- For now, we pass in Math.random() from JavaScript. At some point,
 -- we should get a better random number generator.
-init : Int -> (Model, Cmd Msg)
+
+
+init : Int -> ( Model, Cmd Msg )
 init datRando =
-    ( initialModel (Random.initialSeed datRando) , Cmd.none )
+    ( initialModel (Random.initialSeed datRando), Cmd.none )
+
+
 
 -- if paused, stop sending animation frame updates
+
+
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    if model.paused == False then
-        onAnimationFrameDelta Frame
-    else
+    if model.gameOver == True then
         Sub.none
+
+    else if model.paused == False then
+        Sub.batch [ onVisibilityChange VisibilityChange, onAnimationFrameDelta Frame ]
+
+    else
+        onVisibilityChange VisibilityChange
+
 
 main : Program Int Model Msg
 main =
